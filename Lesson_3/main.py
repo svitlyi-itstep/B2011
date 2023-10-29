bdea5e829124dcce724c030ba9e4e434e3417001
svitlyi <svitlyi_a@itstep.academy>
2023-10-29 11:45:38 +0200

    Добавлен код борьбы двух персонажей

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,1 +1,23 @@
-print('Hello world!')
+from character import Character
+from berserk import Berserk
+
+player1 = Berserk(name='Петя', health=100,
+                    damage=10, defence=0)
+player2 = Character('Вася')
+
+print(player1)
+print(player2)
+
+for _ in range(3):
+    player1_dmg = player1.attack(player2)
+    player2_dmg = player2.attack(player1)
+
+    print(f'{player1.name} атаковал {player2.name} и нанёс {player1_dmg} урона.')
+    print(f'У {player2.name} осталось {player2.health} здоровья.')
+    print(f'{player2.name} атаковал {player1.name} и нанёс {player2_dmg} урона.')
+    print(f'У {player1.name} осталось {player1.health} здоровья.')
+    print()
+
+print(player1)
+print(player2)
+
